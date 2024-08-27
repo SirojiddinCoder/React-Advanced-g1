4e48b827f35f8024ca8e3e7242186d38ce83f6ea
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-08-27 09:53:41 -0700

    reducer counter

--- UseReducer1/src/Reducer1.jsx
+++ UseReducer1/src/Reducer1.jsx
@@ -8,6 +8,7 @@ export const Reducer1 = () => {
         switch(action.type){
             case  'plus': return state + 1;
             case 'minus' : return state - 1;
+            case 'byamount' : return state + action.payload;
             default : return state;
         }
           
@@ -23,6 +24,9 @@ export const Reducer1 = () => {
         <h2>Counter:{counter}</h2>
         <button onClick={()=>dispatch({type:"minus"})}>-</button>
         <button onClick={()=>dispatch({type:"plus"})}>+</button>
+        <button onClick={()=>dispatch({type:"byamount",payload:5})}>5</button>
+        <button onClick={()=>dispatch({type:"byamount",payload:10})}>10</button>
+        <button onClick={()=>dispatch({type:"byamount",payload:15})}>15</button>
     </div>
   )
 }
